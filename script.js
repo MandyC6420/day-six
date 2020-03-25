82dc44ec3ad931acbd0427be6dd1ceb535380ffe
Mandy <drgnfli6420@yahoo.com>
2020-03-25 14:56:09 -0400

    practice

---
+++ script.js
@@ -0,0 +1,98 @@
+
+
+// Write a function that logs the string "I love JavaScript!" to the console.
+// Call the function.
+
+
+
+
+
+function sayIloveJavaScript(){
+    console.log("I love JavaScript!");
+  }
+
+  sayIloveJavaScript();
+
+  function greetAFriend(friendName){
+    console.log(`Hello, ${friendName}!`)
+  }
+
+  greetAFriend("Dwayne");
+
+//   function greetAFriend(friendName, timeOfDay){
+    // var greeting;
+    // if(timeOfDay === "morning"){
+    //   greeting = "Good morning";
+    // } else if (timeOfDay === "afternoon"){
+    //   greeting = "Good afternoon";
+    // } else if (timeOfDay === "evening"){
+    //   greeting = "Good evening";
+    // } else {
+    //   greeting = "Hello";
+    // }
+    // var personalGgreeting = `${greeting}, ${friendName}!`;
+    // console.log(personalGreeting);
+
+//     greetAFriend("Todd", "evening"); // Good evening, Todd!
+// greetAFriend("Jamie", "morning"); // Good morning, Jamie!
+// greetAFriend("Hannah", "afternoon"); // Good afternoon, Hannah!
+// greetAFriend("Norman", "midnight"); // Hello, Norman!
+
+  function greetAFriend(friendName, timeOfDay){
+      var greeting;
+      if(timeOfDay === "morning"){
+          greeting = "Buenas Dia!"
+         }else if (timeOfDay === "afternoon"){
+             greeting = "Buenas Tardes!"
+         }else if (timeOfDay === "evening"){
+          greeting = "Buena Noches!"
+         }
+         var personalGreeting = `${greeting}, ${friendName}!`;
+         function greetAFriend(timeOfDay: afternoon: friendName: Mike)
+
+        }
+
+        // Write a function that accepts two parameters of name and destination.
+        // The function should log a sentence to the console about where that person wants to go.
+        // Call the function three different times with different parameters. Example: if you pass in 
+        // "Jessica" and "Mount Fuji" as parameters, you should see the sentence 
+        // "Jessica would love to visit Mount Fuji." logged to the console.
+    
+        function vacation(friendName, destination){
+        var greeting:
+        if(friendName === Jessica)
+            console.log(friendName = Jessica, destination = MountFuji)
+      }
+
+      // function makeAPizza(crustType, size, toppingsArray){
+      //   var finishedPizza = `A ${size} pizza with ${crustType} crust and ${toppingsArray.join(", ")} on top.`
+      //   return finishedPizza;
+      // }
+      
+      // var myPizza = makeAPizza("wheat", "large", ["green peppers", "onions", "pepperoni"]);
+      
+      // var yourPizza = makeAPizza("white", "medium", ["anchovies", "feta", "pineapple"]);
+      
+      // console.log(myPizza);
+      // // Expected output: "A large pizza with wheat crust and green peppers, onions, pepperoni on top."
+      
+      // console.log(yourPizza);
+      // // Expected output: "A medium pizza with white crust and green anchovies, feta, pineapple on top."
+
+//       Write a function to represent a taco truck's ordering system. The function should accept two parameters: typeOfShell and topping. (Both will be strings.)
+// Inside the function, use string interpolation to build a sentence that announces the taco is ready. Use the information the customer passed in. For example: "Your soft shell taco with chicken is ready!"
+// Return the taco sentence.
+// Execute the function.
+// Log the returned sentence to the console.
+
+      function tacoTruck(shellType, toppingsArray){
+        var finishedTaco = `Your ${shellType} taco with ${toppingsArray};`
+        return finishedTaco;
+      }
+      var myTaco = finishedTaco(soft, [tomatoes, cheese, sour cream])
+      
+      function add(5,5);
+      function subtract(4,1);
+      function multiply(5,4);
+      function divide(2,6);
+
